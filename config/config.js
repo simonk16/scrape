87b659b15544d8e18f5a771a654c1ea48e6f8a77
Kraus, Simon <simon.kraus@aol.com>
2019-08-26 16:06:52 -0500

    db config and server

---
+++ config/config.js
@@ -0,0 +1,11 @@
+var users = [
+        {
+        "_id": "heroku_65j1p7bj.user1",
+        "user": "user1",
+        "db": "heroku_65j1p7bj",
+        "roles": [{
+            "role": "dbOwner",
+            "db": "heroku_65j1p7bj"
+        }]
+    }
+]
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var logger = require("morgan");
+var exphbs  = require('express-handlebars');
 var app = express();
 
 
@@ -11,8 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+
+var PORT = process.env.PORT || 3000;
 
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
