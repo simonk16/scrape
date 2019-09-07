1374d84ac84d973f2eae8e9e46be9dae81ad31e7
Kraus, Simon <simon.kraus@aol.com>
2019-09-07 01:45:19 -0500

    working on modal functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-console.log("This app is serving static files!");
+$(document).ready(function(){
+    console.log("This app is serving static files!");
+    $("#modalPopper").click(function(){
+        console.log('hello')
+    
+        
+    })
+})
